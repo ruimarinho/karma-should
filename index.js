6f123737b0f6bbf524d279825b7f80463ef10d3d
Karl Fleischmann <fleischmann.karl@gmail.com>
2015-07-24 01:52:22 +0100

    Reflect changes from should.js v7.x.x
    In version 7.0.0 the main file in the package.json of should.js changed
    from './lib/should.js' to './index.js'. Due to the decreased directory
    depth, the path-building routine for karma didn't point to a valid
    location.
    
    State lowest version of should.js that introduced the restructuring and
    conditionally reduce the path's depth by 1.

--- index.js
+++ index.js
@@ -3,28 +3,39 @@
  * Module dependencies.
  */
 
+var fs = require('fs');
 var path = require('path');
 
 /**
  * File pattern for `karma`.
  */
 
-var pattern = function(file) {
+function pattern(file) {
   return {
     pattern: file,
     included: true,
     served: true,
     watched: false
   };
-};
+}
 
 /**
  * Framework definition for `karma`.
  */
 
-var framework = function(files) {
-  files.unshift(pattern(path.resolve(require.resolve('should'), '../../should.js')));
-};
+function framework(files) {
+  var should = require.resolve('should');
+  var basename = path.basename(should, '.js');
+
+  // should =< 6
+  if (['should', 'node'].indexOf(basename) !== -1) {
+    should = path.resolve(should, path.join('..', path.sep, '..', path.sep, 'should.js'));
+  } else {
+    should = path.resolve(should, path.join('..', path.sep, 'should.js'));
+  }
+
+  files.unshift(pattern(should));
+}
 
 /**
  * Inject `config.files` on framework.
@@ -38,4 +49,4 @@ framework.$inject = ['config.files'];
 
 module.exports = {
   'framework:should': ['factory', framework]
-};
+}
